641e2803a4bfe4d677adeb61e9b1b682cebd7b77
gregor <gregor.mitscha-baude@gmx.at>
2020-08-14 09:23:20 +0200

    finish v1

--- convertImage.sh
+++ convertImage.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
-# convert images to resized png and webp
-
+# convert image to webp in medium & small version
 FILE=$1
 NAME="${FILE%.*}"
 
+# convert to png & resize
 convert $FILE -resize 400 "$NAME.png"
 convert $FILE -resize 100 "$NAME-small.png"
-cwebp -lossless "$NAME.png" -o "$NAME.webp"
-cwebp -lossless "$NAME-small.png" -o "$NAME-small.webp"
+
+# convert resized pngs to webp and save to public folder
+cwebp -lossless "$NAME.png" -o "public/$NAME.webp"
+cwebp -lossless "$NAME-small.png" -o "public/$NAME-small.webp"
+
+# clean up
+rm "$NAME.png" "$NAME-small.png"
